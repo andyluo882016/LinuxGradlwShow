3232994c9baa77292117fd8304363422236f4dc3
andyluo882016 <andyluo882016@gmail.com>
2022-05-07 13:03:25 -0700

    first commt v0 @ 5/7

---
+++ LinuxGradlwShow/src/main/java/com/app/MainTest.java
@@ -0,0 +1,8 @@
+package com.app;
+
+public class MainTest {
+//https://github.com/andyluo882016/LinuxGradlwShow.git
+    public static void main(String[] args){
+        System.out.println("Show Demo");
+    }
+}
